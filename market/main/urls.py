a2c7f0608c17d9a2e106f8aacb900a21b53dbaf7
Atahualpa Duran <ajdm199231@gmail.com>
2021-09-30 00:58:20 -0400

    first commit

---
+++ market/main/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from main import views
+
+urlpatterns = [
+    
+    path('', views.homepage, name='home'),
+    path('home/', views.homepage, name='home'),
+    path('items/', views.itemspage, name='items'),
+    path('login/', views.loginpage, name='login'),
+    path('logout/', views.logoutpage, name='logout'),
+    path('register/', views.registerpage, name='register')
+    
+]
